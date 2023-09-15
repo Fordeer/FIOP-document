22ccba1e18506284dc0f376ab8878f9c550fd6a8
Alex <dinhhungvn@gmail.com>
2023-09-15 16:47:48 +0700

    Update sale pop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/fordeer-invoice-order-printer/introduction"
+            to="/docs/fordeer-invoice-order-printer/fordeer-pdf-invoice-generator/introduction/"
           >
             Get started - 5min ⏱️
           </Link>
